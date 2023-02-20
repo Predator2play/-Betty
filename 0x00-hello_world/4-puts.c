0cdcf122105eb902046c0842c8800abe2aae9216
Neows <meows@gmail>
2023-02-20 02:38:45 -0800

    0-preprocessor

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *main - main (does not recall fuction) success
+ *return - always 0 (success)
+ */
+int main(void)
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
