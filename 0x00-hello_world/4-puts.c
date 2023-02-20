aa232a0ab91d9b495d472bbafd44015f2c84b423
Neows <meows@gmail>
2023-02-20 03:22:16 -0800

    4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - A program that prints a line with puts function
+ *main - A function that prints a line with the puts function
  *return: 0 (success)
  */
 int main(void)
