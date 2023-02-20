ff6ddd303bedf0db3a9a1bbcb53dcb85ebf1fb1c
Neows <meows@gmail>
2023-02-20 03:11:46 -0800

    4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- *main - main (does not recall fuction) success
- *return - always 0 (success)
+ *main - A program that prints a line with puts function
+ *return: 0 (success)
  */
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
