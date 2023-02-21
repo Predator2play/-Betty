9d54b7544ee77a02494ac6681898bb69216f5c8c
Neows <meows@gmail>
2023-02-21 00:02:26 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints exactly one line followed bya new line, to the standard output
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+	return (1);
+}
