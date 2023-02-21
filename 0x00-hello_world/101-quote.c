49034648013ef2792702756bd5d22556198441f5
Neows <meows@gmail>
2023-02-21 00:24:15 -0800

    New 4-puts.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
- * main - A program that prints exactly one line followed bya new line, to the standard output
- * Return: 1 (Success)
+ *main - A program that prints exactly one line followed bya new line, to the standard output
+ *Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+	write(STDOUT FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, 59);
 	return (1);
 }
