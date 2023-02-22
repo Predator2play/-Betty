f80288b7aeb6ddc8ff67e81bb5c6998c4283a9be
Neows <meows@gmail>
2023-02-22 04:05:38 -0800

    New Thanks

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - A programm that prints numbers of base 16
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 48;
+	int m = 97;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 102)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
