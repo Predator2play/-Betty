9de286afe32cceebe13ccc6420a4829ca849e45e
Neows <meows@gmail>
2023-02-23 01:06:57 -0800

    Oat

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,8 +16,8 @@ int main(void)
 		if (m > 5)
 			printf("last digit of %d is %d and is greater than 5\n", n, m);
 		if (m == 0)
-			printf("last digit of %d is %d and is equal to 0\n", n, m);
-		if (m < 6 && m != 0)
+			printf("last digit of %d is %d and is  0\n", n, m);
+		if (m < 6 && m != 0)i
 			printf("last digit of %d is %d and is less than 6 not 0\n", n, m);
 	return (0);
 }
