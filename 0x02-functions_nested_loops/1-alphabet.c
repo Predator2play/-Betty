885ec03f0a9a67af73cfe3c94626d3b395c90976
Neows <meows@gmail>
2023-02-24 02:18:02 -0800

    Functions and loops 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *main - A program that prints alphabet in lowercase
+ *void print_alphabet - A program that prints alphabet in lowercase
  *Return: 0 (success)
  */
 void print_alphabet(void)
