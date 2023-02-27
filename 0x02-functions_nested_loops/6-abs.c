0879d2a4a706d49e66fb064dfdde1a956713a87d
Neows <meows@gmail>
2023-02-27 02:30:23 -0800

    Nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,12 +6,12 @@
  */
 int _abs(int n)
 {
-	if (n < 0)
+	if (n >= 0)
 	{
-		int abs_val;
-
-		abs_val = n * -1;
-		return (abs_val);
+		return (n);
 	}
-	return (0);
+	else
+	{
+		return (n * -1);
+	}
 }
