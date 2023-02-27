da5d904d6b8da3f3c83f9f3c54d66199083e5347
Neows <meows@gmail>
2023-02-27 04:23:26 -0800

    Clock 24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- *jack_bauer - A function that prints every minute of the day 
+ *jack_bauer - A function that prints every minute of the day
  *Return: void
  */
 void jack_bauer(void)
 {
-	int a, b, c, d ;
+	int a, b, c, d;
 
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
-			if ((a <= 1 && b <=9) || (a <= 2 && b <= 3))
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
 				for (c = 0; c <= 5; c++)
 				{
