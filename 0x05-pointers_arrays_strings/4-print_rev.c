75a00792992d6d2c75a484a652099c7c6439ce6c
Neows <meows@gmail>
2023-03-01 00:54:28 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_rev - Function that prints a string in reverse
+ *@s: string input
+ *Return: always 0
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	for (a -= 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
